b4ea6f45f93ab3a3f3030313287d5847fe347786
Tarun215 <tarun.cer18@itbhu.ac.in>
2021-06-17 08:49:51 +0530

    style: added formatter to maintain code

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -3,22 +3,23 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name=serializers.CharField(min_length=2,max_length=100)
-    last_name=serializers.CharField(min_length=2,max_length=100)
-    email=serializers.EmailField(min_length=6,max_length=255)
-    password=serializers.CharField(min_length=6,max_length=65,write_only=True)
-
+    first_name = serializers.CharField(min_length=2, max_length=100)
+    last_name = serializers.CharField(min_length=2, max_length=100)
+    email = serializers.EmailField(min_length=6, max_length=255)
+    password = serializers.CharField(
+        min_length=6, max_length=65, write_only=True
+    )
 
     class Meta:
         model = User
-        fields=['first_name','last_name','email','password']
+        fields = ['first_name', 'last_name', 'email', 'password']
 
-    def validate(self,data):
-        email=data.get('email','')
+    def validate(self, data):
+        email = data.get('email', '')
         if User.objects.filter(email=email).exists():
-            raise serializers.ValidationError({'email':('Email Already Exists')})
+            raise serializers.ValidationError(
+                {'email': ('Email Already Exists')})
         return super().validate(data)
 
-    def create(self,validated_data):
+    def create(self, validated_data):
         User.objects.create_user(**validated_data)
-
