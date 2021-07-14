cd37a414e80042ce5f3873a52aef91d795a7e075
Jatin Vats <jatinvats525@gmail.com>
2021-07-14 23:08:44 +0530

    updated

--- PaymentCRM/settings.py
+++ PaymentCRM/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-#7n@bu!9)b5op(w8!=3h2tu&q4o3$g6^q-q4c6+cmzy!1w!g18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     'employee',
     'account',
     'invoice',
+    'corsheaders',
     'credit_notes',
 ]
 
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
